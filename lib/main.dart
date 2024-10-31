a5cd66d08ca6f2f244954f0209659a4b081644ba
Toyosi Jayeola <jeremiahjayeola@gmail.com>
2024-10-31 03:52:59 +0100

    Finished

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade700,
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void updateImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: updateImage,
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
